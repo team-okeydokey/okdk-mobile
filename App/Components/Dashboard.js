bdc6037d05a8ec8b50653d4d3fd6074570aeb729
Seongmin Park <seongminnpark@gmail.com>
2018-07-02 02:54:16 +0900

    Change dashboard title according to state

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -4,21 +4,36 @@ import { View, ScrollView, Text, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { Metrics } from '../Themes/'
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
+
+import DashboardActions, { getTabNameByIndex } from '../Redux/DashboardRedux'
 
 import SlidingUpPanel from 'rn-sliding-up-panel';
 
 // Styles
 import styles from './Styles/DashboardStyle'
 
-export default class DashBoardScreen extends Component {
+class DashBoard extends Component {
 
   // Prop type warnings
   static propTypes = {
     active: PropTypes.bool.isRequired,
     onHeaderPress: PropTypes.func.isRequired,
     headerHeight: PropTypes.number.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this._setHeader = this._setHeader.bind(this);
+  }
+
+  _setHeader() {
+    // if (this.props.dashboardOpen) {
+      let newTitle = getTabNameByIndex(this.props.currentTab);
+      this.setState(previousState => {
+        return { title: newTitle };
+      });
+    // }
   }
 
   render () {
@@ -36,34 +51,39 @@ export default class DashBoardScreen extends Component {
         draggableRange={{
           "top": Metrics.screenHeight,
           "bottom": this.props.headerHeight
-        }}>
+        }}
+        // onDrag={this._setHeader}
+        >
         <View style={styles.panelView}>
-          <TouchableOpacity 
+          <TouchableOpacity
             onPress={this.props.onHeaderPress}>
             <Text style={styles.dashboardHeader}>
-              Dashboard
+              {this.props.title}
             </Text>
           </TouchableOpacity>
           <View style={styles.dashboardTabContainer}>
-            <TouchableOpacity style={styles.dashboardTabButton}> 
+            <TouchableOpacity style={styles.dashboardTabButton} 
+              onPress={() => this.props._selectTab(0)}> 
               <View style={styles.dashboardTabButtonIcon}> 
                 <Icon name="list" size={30} color="#2d2d2d"/>
               </View>
-              <Text numberOfLines={1}>Booking Info</Text>
+              <Text numberOfLines={1}>{getTabNameByIndex(0)}</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.dashboardTabButton}>
+            <TouchableOpacity style={styles.dashboardTabButton}
+              onPress={() => this.props._selectTab(1)}>
               <View style={styles.dashboardTabButtonIcon}> 
                 <Icon name="key" size={30} color="#2d2d2d"/>
               </View>
-              <Text numberOfLines={1}>Doorlock Settings</Text>
+              <Text numberOfLines={1}>{getTabNameByIndex(1)}</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.dashboardTabButton}> 
+            <TouchableOpacity style={styles.dashboardTabButton}
+              onPress={() => this.props._selectTab(2)}> 
               <View style={styles.dashboardTabButtonIcon}> 
                 <Icon name="bell" size={30} color="#2d2d2d"/>
               </View>
-              <Text numberOfLines={1}>Door Activity</Text>
+              <Text numberOfLines={1}>{getTabNameByIndex(2)}</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -71,3 +91,22 @@ export default class DashBoardScreen extends Component {
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    dashboardOpen: state.dashboard.open,
+    title: state.dashboard.title,
+    currentTab: state.dashboard.currentTab
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    _selectTab: (index) => {
+      console.log(index)
+      dispatch(DashboardActions.selectTab(index));
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashBoard)
--- App/Redux/DashboardRedux.js
+++ App/Redux/DashboardRedux.js
@@ -5,7 +5,8 @@ import Immutable from 'seamless-immutable'
 
 const { Types, Creators } = createActions({
   open: null,
-  close: null
+  close: null,
+  selectTab: ['index']
 })
 
 export const AuthTypes = Types
@@ -14,7 +15,9 @@ export default Creators
 /* ------------- Initial State ------------- */
 
 export const INITIAL_STATE = Immutable({
-  open: false
+  open: false,
+  currentTab: 0,
+  title: "Dashboard",
 })
 
 /* ------------- Reducers ------------- */
@@ -23,13 +26,29 @@ export const open = (state) => state.merge({open: true })
 
 export const close = (state) => state.merge({open: false })
 
+export const selectTab = (state, {index}) => 
+  state.merge({currentTab: index,  title: getTabNameByIndex(index)})
+
 /* ------------- Hookup Reducers To Types ------------- */
 
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.OPEN]: open,
   [Types.CLOSE]: close,
+  [Types.SELECT_TAB]: selectTab
 })
 
 /* ------------- Selectors ------------- */
 
 export const isOpen = (dashboardState) => dashboardState.open
+
+export const getCurrentTab = (dashboardState) => dashboardState.currentTab
+
+export const getTabNameByIndex = (index) => {
+
+  switch (index) {
+    case 0: return "Booking Info";
+    case 1: return "Doorlock Settings";
+    case 2: return "Door Activity";
+    defult: return "";
+  }
+}
