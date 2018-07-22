26e301e2ef3887deef9b0ba86aae0b1429ecd868
Seongmin Park <seongminnpark@gmail.com>
2018-07-23 02:44:26 +0900

    Fix activities

--- App/Components/Styles/ActivityStyle.js
+++ App/Components/Styles/ActivityStyle.js
@@ -3,8 +3,8 @@ import { Colors, Fonts, Metrics, ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
   container: {
-    ...ApplicationStyles.shadow,
-    overflow:'visible',
+    // ...ApplicationStyles.shadow,
+    // overflow:'visible',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
@@ -13,6 +13,8 @@ export default StyleSheet.create({
     marginVertical: 10,
     marginHorizontal: 10,
     borderRadius: 10,
+    borderColor: Colors.shadow,
+    borderWidth: 1, 
   },
 
   staffInfoContainer: {
@@ -30,6 +32,7 @@ export default StyleSheet.create({
   },
 
   timestamp: {
-    ...Fonts.style.normal,
+    fontFamily: Fonts.type.base,
+    fontSize: Fonts.size.small
   }
 })
